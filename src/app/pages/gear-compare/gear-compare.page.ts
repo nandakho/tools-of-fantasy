dd5ffac621e0c998a51d667d1e543dde755097d8
nandakho <anandakhosuri@yahoo.com>
2023-07-18 16:22:58 +0700

    add menu icon & equipment progress

--- src/app/pages/gear-compare/gear-compare.page.ts
+++ src/app/pages/gear-compare/gear-compare.page.ts
@@ -7,13 +7,38 @@ import { gear } from 'src/app/services';
   styleUrls: ['./gear-compare.page.scss'],
 })
 export class GearComparePage implements OnInit {
-  yourStats = {
+  elementSelected:elementAvailable = "Flame";
+  elementAvailable:elementAvailable[] = ["Flame","Frost","Physical","Volt","Altered"];
+  yourStats:stats = {
     baseAtk: 0,
     percentAtk: 0
   }
+  equipment: any[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  changeElement(){
+    console.log(`Popup element selection here!`);
+  }
+
+  help(section:string){
+    console.log(`Popup help about: ${section}`);
+  }
+
+  addEquipment(){
+    this.equipment.push({type:"tes"});
+  }
+
+  removeEquipment(index:number){
+    this.equipment.splice(index,1);
+    console.log(this.equipment);
+  }
+}
+
+type elementAvailable = "Flame"|"Frost"|"Physical"|"Volt"|"Altered";
+type stats = {
+  baseAtk: number,
+  percentAtk: number
 }
