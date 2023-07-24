8ee5e48874be5d2b18d32a007457f77411aafb45
nandakho <anandakhosuri@yahoo.com>
2023-07-24 16:28:34 +0700

    compare is now functional

--- src/app/pages/gear-compare/gear-compare.page.ts
+++ src/app/pages/gear-compare/gear-compare.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Preferences } from '@capacitor/preferences';
+import { AlertController, AlertInput, AlertOptions } from '@ionic/angular';
 import { augStat, gear, gearTypes } from 'src/app/services';
 
 @Component({
@@ -22,10 +23,13 @@ export class GearComparePage {
     bonusAtk: 0
   }
   equipment: eqStats[] = [];
-  constructor() { }
+  constructor(
+    private alert: AlertController
+  ) { }
 
   async ionViewWillEnter(){
-    console.log("HAI");
+    //This is for future update cz i'm lazy lol
+    //load last input stats automatically on page load
     try {
       const data = await this.readStorage();
       console.log(data);
@@ -34,8 +38,35 @@ export class GearComparePage {
     }
   }
 
-  changeElement(){
-    console.log(`Popup element selection here!`);
+  eleRadioGenerate(){
+    var r:AlertInput[] = [];
+    for(let i = 0; i<this.elementAvailable.length; i++){
+      r.push({
+        label: this.elementAvailable[i],
+        type:"radio",
+        value: this.elementAvailable[i],
+        checked: this.elementAvailable[i]==this.elementSelected
+      });
+    }
+    return r;
+  }
+
+  async changeElement(){
+    const alert = await this.alert.create({
+      header: "Select Element",
+      backdropDismiss: true,
+      inputs: this.eleRadioGenerate(),
+      buttons: [{
+        text:'OK',
+        handler: async (newSelected)=>{
+          this.elementSelected = newSelected;
+        }
+      },{
+        text:'Cancel'
+      }]
+
+    });
+    alert.present();
   }
 
   changeType(){
@@ -47,18 +78,46 @@ export class GearComparePage {
   }
 
   addEquipment(){
-    this.equipment.push({attack:0,eleAttack:0,eleAttackPercent:0,totalAttack:0});
+    this.equipment.push({attack:0,eleAttack:0,eleAttackPercent:0,totalAttack:this.calcAtk(0,0,0)});
   }
 
   removeEquipment(index:number){
     this.equipment.splice(index,1);
-    console.log(this.equipment);
+  }
+
+  recalcEquip(i:number|"all"){
+    if(i=="all"){
+      if(this.equipment.length>0){
+        for(let id=0;id<this.equipment.length;id++){
+          this.equipment[id].totalAttack = this.calcAtk(this.equipment[id].attack,this.equipment[id].eleAttack,this.equipment[id].eleAttackPercent);
+        }
+      }
+    } else {
+      this.equipment[i].totalAttack = this.calcAtk(this.equipment[i].attack,this.equipment[i].eleAttack,this.equipment[i].eleAttackPercent);
+    }
+  }
+
+  calcAtk(eqAtk:number,eqEleAtk:number,eqElePercent:number){
+    const curPercent = this.yourStats.bonusAtk/this.yourStats.baseAtk;
+    const a = this.yourStats.baseAtk+(this.sharedStats.baseAtk+this.sharedStats.bonusAtk+this.sharedStats.enhanceAtk);
+    const b = eqAtk+eqEleAtk;
+    const result = Math.ceil((a+b)*(1+(eqElePercent/100)+curPercent));
+    return result;
   }
 
   async readStorage(){
     try {
-      const data = await Preferences.get({key:"stats"});
+      const data = await Preferences.get({key:"gear_compare"});
       return Promise.resolve(data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  async saveStorage(data:string){
+    try {
+      await Preferences.set({key:"gear_compare",value:data});
+      return Promise.resolve();
     } catch (err) {
       return Promise.reject(err);
     }
